02991335efa615879a19878ded12878ce3b4d1ed
Elvin <ttop34@icloud.com>
2017-10-03 09:41:57 -0500

    final fix done

--- src/Author.java
+++ src/Author.java
@@ -5,18 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private ArrayList<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
      * @deprecated Use publishedBooks instead
      */
+
     @Deprecated
-    public List<String> getBooks() {
+    public ArrayList<String> getBooks() {
         return books;
     }
 
